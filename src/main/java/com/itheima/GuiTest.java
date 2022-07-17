4bde25239be7c5ad13c00b3bbae0006b012a61b5
king <3235781823@qq.com>
2022-07-17 10:22:49 +0800

    first commit

---
+++ src/main/java/com/itheima/GuiTest.java
@@ -0,0 +1,8 @@
+package com.itheima;
+
+public class GuiTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello g");
+    }
+}
