505217ddd17e1c829c7c2d1f7750db6a61c4f85f
king <3235781823@qq.com>
2022-07-17 10:24:35 +0800

    second commit

--- src/main/java/com/itheima/GuiTest.java
+++ src/main/java/com/itheima/GuiTest.java
@@ -2,7 +2,7 @@ package com.itheima;
 
 public class GuiTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello g");
+        System.out.println("hello git2");
+
     }
 }
